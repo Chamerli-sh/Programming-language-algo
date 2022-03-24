715d4f610d88780f7f39edb3cbb20b5d64dedd18
Chamerli-sh <fares.setbels@gmail.com>
2022-03-24 20:00:40 +0100

    Initial Commit

---
+++ index.js
@@ -0,0 +1,3 @@
+const PL_WBRACK = ['Javascript', 'Java', 'Rust']; // Programming Language with brackets
+const PL_WDP = ['Python'] // Programming Language with the double points
+const PL_CUSED = ['Javascript', 'Java', 'Python']
